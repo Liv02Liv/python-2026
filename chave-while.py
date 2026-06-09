cef5723070b73cd73724cb749923128291568163
Livia Rocha <liviarocha640@gmail.com>
2026-06-09 10:23:07 -0300

    Regressiva

--- chave-while.py
+++ chave-while.py
@@ -26,3 +26,11 @@ def procure_pela_chave(caixa):
 def regressiva(i):
     print(i)
     regressiva(i-1)
+
+#%%
+def regressiva(i):
+    print(i)
+    if i <= 1:
+        return
+    else:
+        regressiva(i-1)
