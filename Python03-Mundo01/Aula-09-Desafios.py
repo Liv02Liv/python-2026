de065ff89bdc19c46b9105c73e658cb6c5f17d1b
Livia Rocha <liviarocha640@gmail.com>
2026-07-02 09:33:53 -0300

    desafio 026

--- Python03-Mundo01/Aula-09-Desafios.py
+++ Python03-Mundo01/Aula-09-Desafios.py
@@ -47,3 +47,19 @@ print("DESAFIO 025")
 
 nome = str(input("Qual é seu nome completo? ")).strip()
 print(f"Seu nome tem Silva? {"silva"in nome.lower()}")
+
+#%%
+#Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, 
+#em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+print("DESAFIO 026")
+
+frase = str(input("Digite uma frase: ")).upper().strip()
+
+print(f"A letra A aparece {frase.count("A")} vezes na frase.")
+print(f"A primeira letra A apareceu na posição {frase.find("A")+1}")
+print(f"A última letra A apareceu na posição {frase.rfind("A")+1}")
+
+#%%
+#
+
