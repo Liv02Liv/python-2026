7c37e7196bd6aa7c03788f28cbb8b81fca5dbb22
Livia Rocha <liviarocha640@gmail.com>
2026-07-02 09:46:09 -0300

    desafio 027

--- Python03-Mundo01/Aula-09-Desafios.py
+++ Python03-Mundo01/Aula-09-Desafios.py
@@ -61,5 +61,15 @@ print(f"A primeira letra A apareceu na posição {frase.find("A")+1}")
 print(f"A última letra A apareceu na posição {frase.rfind("A")+1}")
 
 #%%
-#
+#Faça um programa que leia o nome completo de uma pessoa, 
+#mostrando em seguida o primeiro e o último nome separadamente.
+
+print("DESAFIO 027")
+
+n = str(input("Digite seu nome completo: ")).strip()
+nome = n.split()
+
+print("Muito prazer em te conhecer!")
+print(f"Seu primeiro nome é {nome[0]}")
+print(f"Seu ultimo nome é {nome[-1]}")
 
