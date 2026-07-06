f4e96d5706748f59f9430656abb80b0da791b83c
Livia Rocha <liviarocha640@gmail.com>
2026-07-06 10:14:39 -0300

    desafio-029

--- Python03-Mundo01/Aula-10-Desafios.py
+++ Python03-Mundo01/Aula-10-Desafios.py
@@ -20,3 +20,19 @@ if jogador == computador:
     print("PARABÉNS! Você conseguiu me vencer!")
 else:
     print("GANHEI! Tente outra vez!")
+
+#%%
+#Escreva um programa que leia a velocidade de um carro. 
+#Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
+#A multa vai custar R$7,00 por cada Km acima do limite.
+
+print("DESAFIO 029")
+
+velocidade = float(input("Qual é a velocidade atual do carro? "))
+
+if velocidade > 80:
+    print("MULTADO! Você excedeu o limite permitido que é de 80km/h")
+    multa = (velocidade - 80) * 7
+    print(f"Você deve pagar uma multa de R${multa:.2f}!")
+else:
+    print("Tenha um bom dia! Dirija com segurança!")
