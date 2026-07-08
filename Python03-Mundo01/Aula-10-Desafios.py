2d9980080d25119b011f9ea14fee75788ded860d
Livia Rocha <liviarocha640@gmail.com>
2026-07-08 09:58:15 -0300

    desafio-033

--- Python03-Mundo01/Aula-10-Desafios.py
+++ Python03-Mundo01/Aula-10-Desafios.py
@@ -84,3 +84,31 @@ if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print(f"O ano {ano} é BISSEXTO")
 else:
     print(f"O ano {ano} não é BISSEXTO")
+
+#%%
+#Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+print("DESAFIO 033")
+
+a = int(input("Primeiro valor: "))
+b = int(input("Segundo valor: "))
+c = int(input("Terceiro valor: "))
+
+menor = a 
+
+if b < a and b < c:
+    menor = b
+
+if c < a and c < b:
+    menor = c
+
+maior = a 
+
+if b > a and b > c:
+    maior = b
+
+if c > a and c > b:
+    maior = c 
+
+print(f"O menor valor digitado foi {menor}")
+print(f"O maior valor digitado foi {maior}")
