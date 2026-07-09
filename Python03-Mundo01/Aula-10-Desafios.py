d4d384071b905694bf9721993edc9521f2e8b369
Livia Rocha <liviarocha640@gmail.com>
2026-07-09 09:34:37 -0300

    desafio-034

--- Python03-Mundo01/Aula-10-Desafios.py
+++ Python03-Mundo01/Aula-10-Desafios.py
@@ -112,3 +112,19 @@ if c > a and c > b:
 
 print(f"O menor valor digitado foi {menor}")
 print(f"O maior valor digitado foi {maior}")
+
+#%%
+#Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. 
+#Para salários superiores a R$1621,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+print("DESAFIO 034")
+
+salario = float(input("Qual é o salário do funcionário? R$"))
+
+if salario <= 1621:
+    novo = salario + (salario * 15 / 100)
+
+else:
+    novo = salario + (salario * 10 / 100)
+
+print(f"Quem ganhava R${salario:.2f} passa a ganhar R${novo:.2f} agora.")
