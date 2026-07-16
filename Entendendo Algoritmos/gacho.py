68c5d2217605328c4309b4c5e6caeae9b0b12035
Livia Rocha <liviarocha640@gmail.com>
2026-07-16 10:22:09 -0300

    gancho

--- Entendendo Algoritmos/gacho.py
+++ Entendendo Algoritmos/gacho.py
@@ -10,3 +10,29 @@ pais["fim"] = None
 
 #%%
 
+nodo = ache_no_custo_mais_baixo(custos) 
+
+custo = custos[nodo] 
+
+vizinhos = grafo[nodo] 
+
+for n in vizinhos.keys(): 
+    novo_custo = custo + vizinhos[n] if custos[n] > novo_custo: 
+    custos[n] = novo_custo pais[n] = nodo 
+    
+processados.append(nodo) 
+    
+nodo = ache_no_custo_mais_baixo(custos)
+
+#%%
+def ache_no_custo_mais_baixo(custos): 
+    custo_mais_baixo = float("inf") 
+    nodo_custo_mais_baixo = None 
+    
+    for nodo in custos:
+        custo = custos[nodo] 
+        if custo < custo_mais_baixo and nodo not in processados:
+            custo_mais_baixo = custo
+            nodo_custo_mais_baixo = nodo 
+            
+    return nodo_custo_mais_baixo
