befc64d468d880245660d08aab3fedd149613182
Livia Rocha <liviarocha640@gmail.com>
2026-07-22 10:02:26 -0300

    pizza.sql

--- Data-Drill-Showdowns/pizza_sales/pizza.sql
+++ Data-Drill-Showdowns/pizza_sales/pizza.sql
@@ -1,3 +1,27 @@
 SELECT * FROM price_history;
 
-SELECT * FROM transactions;
+WITH mrd AS (
+SELECT 
+    t.order_detail_id,
+    t.order_date,
+    t.pizza_id,
+    t.quantity,
+    MAX(effective_date) AS most_recent_date
+ FROM transactions t 
+    LEFT JOIN price_history ph 
+        ON t.pizza_id = ph.pizza_id
+        AND t.order_date >= ph.effective_date
+GROUP BY 1, 2, 3, 4
+),
+
+full_transaction_data AS (
+SELECT 
+    m.*,
+    ph.price
+FROM mrd m 
+    LEFT JOIN price_history ph 
+        ON ph.pizza_id = m.pizza_id
+        AND m.most_recent_date = ph.effective_date
+)
+
+SELECT SUM(price * quantity) FROM full_transaction_data;
