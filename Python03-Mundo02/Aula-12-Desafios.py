6e98aee9d1ccc1357ad335212341419cdda547be
Livia Rocha <liviarocha640@gmail.com>
2026-07-28 10:09:14 -0300

    desafio 036

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -4,3 +4,21 @@
 #Exercício Python 36: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. 
 #Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. 
 #A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+
+#%%
+print("DESAFIO 036")
+
+casa = float(input("Valor da casa: R$"))
+salario = float(input("Salário do comprador: R$"))
+anos = int(input("Quantos anos de finacimento? "))
+
+prestacao = casa / (anos * 12)
+minimo = salario * 30 / 100
+
+print(f"Para pagar uma casa de R${casa} em {anos}", end=" ")
+print(f"a prestação será de R${prestacao:.2f}")
+
+if prestacao <= minimo:
+    print("Empréstimo pode ser CONCEDIDO!")
+else:
+    print("Empréstimo NEGADO!")
