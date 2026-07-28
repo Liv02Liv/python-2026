f7127827b07eeae572f631943ab4d9cc9cb5dd53
Livia Rocha <liviarocha640@gmail.com>
2026-07-28 10:19:30 -0300

    enunciado

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -22,3 +22,12 @@ if prestacao <= minimo:
     print("Empréstimo pode ser CONCEDIDO!")
 else:
     print("Empréstimo NEGADO!")
+
+#%%
+
+#Exercício Python 37: Escreva um programa em Python que leia um número inteiro
+#qualquer e peça para o usuário escolher qual será a base de conversão: 
+#1 para binário, 2 para octal e 3 para hexadecimal.
+
+print("DESAFIO 037")
+
