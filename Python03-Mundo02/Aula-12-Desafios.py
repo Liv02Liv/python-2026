928dcb99588af171138c6bc3a672fe8b184eaac1
Livia Rocha <liviarocha640@gmail.com>
2026-07-28 10:31:55 -0300

    desafio 037

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -31,3 +31,20 @@ else:
 
 print("DESAFIO 037")
 
+num = int(input("Digite um número inteiro: "))
+
+print('''Escolha uma das bases para conversão:
+[1] Converter para BINÁRIO
+[2] Converter para OCTAL
+[3] Converter para HEXADECIMAL\n''')
+
+opçao = int(input("Sua opção: "))
+
+if opçao == 1:
+    print(f"{num} convertido para BINÁRIO é igual a {bin(num)[2:]}")
+elif opçao == 2:
+    print(f"{num} convertido para OCTAL é igual a {oct(num)[2:]}")
+elif opçao == 3:
+    print(f"{num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}")
+else:
+    print("Opção inválids, Tente novamente.")
