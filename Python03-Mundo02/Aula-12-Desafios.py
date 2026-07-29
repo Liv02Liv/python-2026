fc8986beb91b6ac0be02c24968aaa4dad5987198
Livia Rocha <liviarocha640@gmail.com>
2026-07-29 10:02:09 -0300

    desafio 038

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -49,8 +49,21 @@ elif opçao == 3:
 else:
     print("Opção inválids, Tente novamente.")
 
+#%%
 
 #Exercício Python 038: Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
 #– O primeiro valor é maior
 #– O segundo valor é maior
 #– Não existe valor maior, os dois são iguais
+
+print("DESAFIO 038")
+
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+
+if n1 > n2:
+    print("O PRIMEIRO valor é maior")
+elif n2 > n1:
+    print("O SEGUNDO valor é maior")
+else:
+    print("Os dois valores são IGUAIS")
