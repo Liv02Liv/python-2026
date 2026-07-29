d7b42d2a0478f3742e2fc5e92e8b22c132ac86e0
Livia Rocha <liviarocha640@gmail.com>
2026-07-29 10:19:32 -0300

    enunciado

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -67,3 +67,29 @@ elif n2 > n1:
     print("O SEGUNDO valor é maior")
 else:
     print("Os dois valores são IGUAIS")
+
+#%%
+
+#Exercício Python 39: Faça um programa que leia o ano de nascimento de um jovem e informe, 
+#de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, 
+#se é a hora exata de se alistar ou se já passou do tempo do alistamento. 
+#Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+print("DESAFIO 039")
+
+from datetime import date
+
+atual = date.today().year
+nasc = int(input("Ano de nascimento: "))
+idade = atual - nasc
+
+print(f"Quem nasceu em {nasc} tem {idade} anos em {atual}.")
+
+if idade == 18:
+    print("Você tem que se alistar IMEDIATAMENTE!")
+elif idade < 18:
+    saldo = 18 - idade 
+    print(f"Ainda faltam {saldo} anos para o alistamento")
+    ano = atual + saldo
+    print(f"Seu alistamento será em {ano}")
+    
