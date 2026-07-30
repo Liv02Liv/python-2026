d55c86a8b554bbf3a6463043d93b3558343ac08f
Livia Rocha <liviarocha640@gmail.com>
2026-07-30 09:55:29 -0300

    enunciado

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -97,3 +97,13 @@ elif idade > 18:
     print(f"Você já deveria ter se alistado há {saldo} anos.")
     ano = atual - saldo 
     print(f"Seu alistamento foi em {ano}")
+
+#%%
+
+#Exercício Python 040: Crie um programa que leia duas notas de um aluno e calcule sua média, 
+# mostrando uma mensagem no final, de acordo com a média atingida:
+#– Média abaixo de 5.0: REPROVADO
+#– Média entre 5.0 e 6.9: RECUPERAÇÃO
+#– Média 7.0 ou superior: APROVADO
+
+print("DESAFIO 040")
