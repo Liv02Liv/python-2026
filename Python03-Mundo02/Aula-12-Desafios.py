ebecbbbf1d727024be8795547a914d9c7c284c2c
Livia Rocha <liviarocha640@gmail.com>
2026-07-31 10:51:55 -0300

    desafio 043

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -188,3 +188,20 @@ else:
 #– Acima de 40: Obesidade Mórbida
 
 print("DESAFIO 043")
+
+peso = float(input("Qual é seu peso? (Kg) "))
+altura = float(input("Qual é a sua altura? (M) "))
+imc = peso / (altura ** 2)
+
+print(f"O IMC dessa pessoa é de {imc:.1f}")
+
+if imc < 18.5:
+    print("Você está ABAIXO DO PESO normal")
+elif 18.5 <= imc < 25:
+    print("PARABÉNS, você está na faixa de PESO NORMAL")
+elif 25 <= imc < 30:
+    print("Você está em SOBREPESO")
+elif 30 <= imc < 40:
+    print("Você está em OBESIDADE")
+elif imc >= 40:
+    print("Você está em OBESIDADE MÓRBIDA, Cuidado!")
