06627457cee7e168f097ba515752a8f5bc65abac
Livia Rocha <liviarocha640@gmail.com>
2026-07-31 09:46:49 -0300

    enunciado

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -151,3 +151,14 @@ elif idade <= 25:
     print("Classificação: SÊNIOR")
 else:
     print("Classificação: MASTER")
+
+#%%
+
+#Exercício Python 42: Refaça o DESAFIO 35 dos triângulos, acrescentando 
+#o recurso de mostrar que tipo de triângulo será formado:
+#– EQUILÁTERO: todos os lados iguais
+#– ISÓSCELES: dois lados iguais, um diferente
+#– ESCALENO: todos os lados diferentes
+
+print("DESAFIO 042")
+
