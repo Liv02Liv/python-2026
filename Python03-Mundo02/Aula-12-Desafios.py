b0cb02ee3d985f811b8cdc57555cdb9c3e6c1ca4
Livia Rocha <liviarocha640@gmail.com>
2026-08-02 20:21:31 -0300

    enunciado

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -205,3 +205,11 @@ elif 30 <= imc < 40:
     print("Você está em OBESIDADE")
 elif imc >= 40:
     print("Você está em OBESIDADE MÓRBIDA, Cuidado!")
+
+#%%
+
+#Exercício Python 44: Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
+#– à vista dinheiro/cheque: 10% de desconto
+#– à vista no cartão: 5% de desconto
+#– em até 2x no cartão: preço formal 
+#– 3x ou mais no cartão: 20% de juros
