42e71f6731565d2b95423a8e0fadb3098c4b2f04
Livia Rocha <liviarocha640@gmail.com>
2026-08-02 21:24:03 -0300

    desafio 044

--- Python03-Mundo02/Aula-12-Desafios.py
+++ Python03-Mundo02/Aula-12-Desafios.py
@@ -213,3 +213,36 @@ elif imc >= 40:
 #– à vista no cartão: 5% de desconto
 #– em até 2x no cartão: preço formal 
 #– 3x ou mais no cartão: 20% de juros
+
+print("DESAFIO 044\n")
+
+print("{:=^40}".format(" LOJAS GUANABARA "))
+
+preço = float(input("Preço das compras: R$"))
+
+print("""FORMAS DE PAGAMENTOS
+[1] à vista dinheiro|cheque
+[2] à vista cartão
+[3] 2x no cartão
+[4] 3x ou mais no cartão""") 
+
+opçao = int(input("Qual é a opção? "))
+
+if opçao == 1:
+    total = preço - (preço * 10 / 100)
+elif opçao == 2:
+    total = preço - (preço * 5 / 100)
+elif opçao == 3:
+    total = preço
+    parcela = total / 2
+    print(f"Sua compra será parcelada em 2x de R${parcela:.2f} SEM JUROS")
+elif opçao == 4:
+    total = preço + (preço * 20 / 100)
+    totparc = int(input("Quantas parcelas? "))
+    parcela = total / totparc
+    print(f"Sua compra será parcelada em {totparc:.2f} de R${parcela:.2f} COM JUROS")
+else:
+    total = preço
+    print("OPÇÃO INVÁLIDA de pagamento, tente novamente!")
+
+print(f"Sua compra de R${preço:.2f} vai custar R${total:.2f} no final.")
