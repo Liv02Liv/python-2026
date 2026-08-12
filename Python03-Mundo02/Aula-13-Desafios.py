65d512a371a6a9a326470e1fcf1430c43ac82aa5
Livia Rocha <liviarocha640@gmail.com>
2026-08-12 09:46:59 -0300

    Enuciado

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -7,3 +7,10 @@
 #print(c)
 
 #print(‘Acabou’)
+
+#%%
+
+#Exercício Python 46: Faça um programa que mostre na tela uma contagem regressiva para 
+#o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+print("DESAFIO 046")
