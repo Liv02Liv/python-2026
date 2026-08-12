24defc17db278fe5701afd02cb733a5ba9a9fef3
Livia Rocha <liviarocha640@gmail.com>
2026-08-12 10:00:45 -0300

    desafio-047

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -27,3 +27,7 @@ print("BUM ! BUM! POOOW!")
 #Exercício Python 47: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
 print("DESAFIO 047")
+
+for n in range(2, 51, 2):
+    print(n, end=" ")
+print("\nAcabou!")
