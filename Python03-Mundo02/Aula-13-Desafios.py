6d26359656f038d36291012c4d590b346f6baa5f
Livia Rocha <liviarocha640@gmail.com>
2026-08-12 09:53:02 -0300

    desafio-046

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -14,3 +14,10 @@
 #o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
 
 print("DESAFIO 046")
+
+from time import sleep
+
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(0.5)
+print("BUM ! BUM! POOOW!")
