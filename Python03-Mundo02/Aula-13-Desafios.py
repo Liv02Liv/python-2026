3dd2f469d9a227087aaaaa16b4b361b8b22c7275
Livia Rocha <liviarocha640@gmail.com>
2026-08-28 10:23:35 -0300

    desafio-049

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -54,3 +54,9 @@ print(f"A soma de todos os {cont} valores solicitados é {soma}")
 #Exercício Python 49: Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, 
 #só que agora utilizando um laço for.
 
+print("DESAFIO 049")
+
+num = int(input("Digite um número para ver sua tabuada: "))
+
+for c in range(1, 11):
+    print(f"{num} x {c:2} = {num*c}")
