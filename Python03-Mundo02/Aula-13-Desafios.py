64fc074bc507e48218d1bdbeb0dec4c2f6a69a03
Livia Rocha <liviarocha640@gmail.com>
2026-08-28 10:12:18 -0300

    desafio-048

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -38,3 +38,13 @@ print("\nAcabou!")
 #Exercício Python 48: Faça um programa que calcule a soma entre todos os números que são múltiplos de três
 #e que se encontram no intervalo de 1 até 500.
 
+print("DESAFIO 048")
+
+soma = 0
+cont = 0
+
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        soma += c 
+        cont += 1
+print(f"A soma de todos os {cont} valores solicitados é {soma}")
