0eea2987abfaa2df3d8fc0f5091747c72bf0f815
Livia Rocha <liviarocha640@gmail.com>
2026-09-14 13:11:38 -0300

    Score

---
+++ informatika.nazrin/Execicios02.py
@@ -0,0 +1,15 @@
+#%%
+
+score = 85
+
+if score > 90:
+    print("Lionel Messi")
+
+elif score > 75 and score < 90:
+    print("Cristiano Ronaldo")
+
+elif score == 100:
+    print("Neymar")
+
+else:
+    print("Kylian Mbappé")
