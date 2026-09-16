ea576050356926dd1a040bf5a10022feaf2f2cf0
Livia Rocha <liviarocha640@gmail.com>
2026-09-16 10:37:23 -0300

    desafio-058

--- Python03-Mundo02/Aula-14-Desafios.py
+++ Python03-Mundo02/Aula-14-Desafios.py
@@ -65,3 +65,33 @@ sexo = str(input("Informe seu sexo: [M/F] ")).strip().upper()[0]
 while sexo not in "MnFf":
     sexo = str(input("Dados inválidos, por favor, informe seu sexo: ")).strip().upper()[0]
 print(f"Sexo {sexo} registrado com sucesso!")
+
+#%%
+
+#Exercício Python 58: Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. 
+#Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+
+print("DESAFIO 058")
+
+from random import randint
+
+computador = randint(0, 10)
+
+print("Sou seu computador... Acabei de pensar em um número entre 0 e 10.")
+print("Será que você consegue adivinhar qual foi? ")
+
+acertou = False
+palpites = 0
+
+while not acertou:
+    jogador = int(input("Qual é o seu Palpite? "))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print("Mais... Tente mais uma vez.")
+        elif jogador > computador:
+            print("Menos.. Tente mais uma vez.")
+
+print(f"Acertou com {palpites} tentativas. Parabéns!")
