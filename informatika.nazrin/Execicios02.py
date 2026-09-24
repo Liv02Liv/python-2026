29061d0d5daa3f2d17017978c6be5901c5e946ed
Livia Rocha <liviarocha640@gmail.com>
2026-09-24 10:45:29 -0300

    Letras

--- informatika.nazrin/Execicios02.py
+++ informatika.nazrin/Execicios02.py
@@ -144,3 +144,25 @@ if "barca" in team.lower():
     print(team[:5])
 else:
     print(team[3:])
+
+
+#%% 
+
+a =7
+b = 3
+c = 12
+
+if a > 5:
+    if b > 5:
+        print("P")
+    elif c > 10:
+        if a + b > c:
+            print("S")
+        else:
+            print("T")
+    else:
+        print("F")
+elif c > a:
+    print("U")
+else:
+    print("P")
