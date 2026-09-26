a769fb1c4ac7c088b229e08e1e04d2d8f1afd358
Livia Rocha <liviarocha640@gmail.com>
2026-09-26 10:40:03 -0300

    check_sentence_ending

--- Exercism/string_methods.py
+++ Exercism/string_methods.py
@@ -6,3 +6,10 @@ def capitalize_title(title):
 print(capitalize_title("my hobbies"))
 
 #👉 .title() coloca a primeira letra de cada palavra em maiúscula.
+
+#%%
+
+def check_sentence_ending(sentence):
+    return "." in sentence
+
+print(check_sentence_ending("I like to hike, bake, and read."))
